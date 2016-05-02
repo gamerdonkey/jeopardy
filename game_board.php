cf40e650cd55ae9a400961c156e302c8992c0c68
gamerdonkey <gamerdonkey@gmail.com>
2016-05-01 22:55:14 -0500

    Initial commit with game board prototype.

---
+++ game_board.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+   <link rel="stylesheet" type="text/css" href="game_board_style.css">
+</head>
+<body>
+<div class="game-board">
+   <ul class="category">
+      <li class="display-box category-title"><div class="category-title-content">Meow moew about meow and also things</div></li>
+      <li class="display-box">$200</li>
+      <li class="display-box">$400</li>
+      <li class="display-box">$600</li>
+      <li class="display-box">$800</li>
+      <li class="display-box">$1000</li>
+   </ul>
+</div>
+</body>
+</html>
