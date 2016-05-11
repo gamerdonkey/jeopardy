ee192916c77aa82af67f01a9ffbb3ee014b2650a
gamerdonkey <gamerdonkey@gmail.com>
2016-05-10 21:37:03 -0500

    Added gitignore and started work on php pulling from sqlite db.

--- game_board.php
+++ game_board.php
@@ -3,15 +3,29 @@
    <link rel="stylesheet" type="text/css" href="game_board_style.css">
 </head>
 <body>
+
 <div class="game-board">
-   <ul class="category">
-      <li class="display-box category-title"><div class="category-title-content">Meow moew about meow and also things</div></li>
-      <li class="display-box">$200</li>
-      <li class="display-box">$400</li>
-      <li class="display-box">$600</li>
-      <li class="display-box">$800</li>
-      <li class="display-box">$1000</li>
-   </ul>
+<?php
+  $questionsDb = new SQLite3('gfm_furry_jeopary_2016.db');
+
+  $selectCategoriesStmt = $questionsDb->prepare('SELECT id, title FROM categories WHERE round = :round');
+  $selectCategoriesStmt->bindValue(':round', $_GET["round"], SQLITE3_INTEGER);
+  $categoriesResult = $selectCategoriesStmt->execute();
+  while($categoryRow = $categoriesResult->fetchArray(SQLITE3_ASSOC)) {
+    ?>
+    <ul class="category">
+       <li class="display-box category-title">
+         <div class="category-title-content"><?php echo $categoryRow['title']; ?></div>
+       </li>
+       <li class="display-box">$200</li>
+       <li class="display-box">$400</li>
+       <li class="display-box">$600</li>
+       <li class="display-box">$800</li>
+       <li class="display-box">$1000</li>
+    </ul>
+    <?php
+  }
+?>
 </div>
 </body>
 </html>
