52bb0e9352f5a84ca8b7ae4bcaa8f542708ee5af
gamerdonkey <gamerdonkey@gmail.com>
2016-09-18 23:16:21 -0500

    Added non-working links to individual category pages and the framework for adding new categories.

--- admin/questions.php
+++ admin/questions.php
@@ -1,23 +1,56 @@
+<?php
+$questionsDb = new SQLite3('../db/mff_2016_questions.db');
+
+if( isset($_POST['category-title']) && !empty($_POST['category-title']) ) {
+
+   $title = htmlentities($_POST['category-title']);
+
+   $preparedStatement = $questionsDb->prepare('INSERT INTO categories (title) VALUES (:title)');
+   $preparedStatement->bindValue('title', title);
+   $result = $preparedStatement->execute();
+   if(!$result) {
+     echo "An error occurred!";
+   }
+}
+
+?>
 <html>
 <head>
+  <title>Leopardy! Catergories</title>
 </head>
 <body>
-  <table border="true">
-  <?php
-    $questionsDb = new SQLite3('../db/mff_2016_questions.db');
 
+  <h1>Leopardy! Categories</h1>
+
+  <table border="true">
+    <tr>
+      <th>ID</th>
+      <th>Title</th>
+      <th>Round</th>
+    </tr>
+  <?php
     $selectCategoriesStmt = $questionsDb->prepare('SELECT rowid, title, round FROM categories');
     $categoriesResult = $selectCategoriesStmt->execute();
     while($categoryRow = $categoriesResult->fetchArray(SQLITE3_ASSOC)) {
       ?>
       <tr>
         <td><?php echo $categoryRow['rowid']; ?></td>
-        <td><?php echo $categoryRow['title']; ?></td>
+        <td><a href="./category.php?id=<?php echo $categoryRow['rowid']; ?>"><?php echo $categoryRow['title']; ?></a></td>
         <td><?php echo $categoryRow['round']; ?></td>
       </tr>
       <?php
     }
   ?>
   </table>
+
+  <fieldset><legend>Add New Category</legend>
+    <form id="new-category-form" action="./questions.php" method="post">
+
+    <input required name="category-title" type="text" class="form-input" placeholder="Category Title" />
+
+    <input type="submit" class="button form-submit" value="Add" />
+
+    </form>
+  </fieldset>
 </body>
 </html>
