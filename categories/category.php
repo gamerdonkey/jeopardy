8dd92088f974958e97d1e1f3a3fb4f074ffe2a73
gamerdonkey <gamerdonkey@gmail.com>
2016-11-14 21:37:13 -0600

    Fleshed out the actual question addition/update db logic.

--- categories/category.php
+++ categories/category.php
@@ -3,27 +3,60 @@ $questionsDb = new SQLite3('../db/mff_2016_questions.db');
 
 if( isset($_GET['id']) && !empty($_GET['id']) ) {
 
-   $categoryId = htmlentities($_GET['id']);
+  $categoryId = htmlentities($_GET['id']);
 
-   $preparedStatement = $questionsDb->prepare('SELECT title from categories where rowid = (:id)');
-   $preparedStatement->bindValue('id', $categoryId);
-   $result = $preparedStatement->execute();
-   if(!$result) {
-     echo "An error occurred!";
-     return 0;
-   }
+  $categoryTitleSelectStatment = $questionsDb->prepare('SELECT title from categories where rowid = (:id)');
+  $categoryTitleSelectStatment->bindValue('id', $categoryId);
+  $categoryTitleResult = $categoryTitleSelectStatment->execute();
+  if(!$categoryTitleResult) {
+    echo "An error occurred!";
+    return 0;
+  }
 
-   $categoryTitle = $result->fetchArray(SQLITE3_ASSOC)['title'];
+  $categoryTitle = $categoryTitleResult->fetchArray(SQLITE3_ASSOC)['title'];
+
+  if( isset($_POST['question-text']) && !empty($_POST['question-text'])
+    && isset($_POST['answer']) && isset($_POST['value']) ) {
+    if( isset($_POST['rowid']) && !empty($_POST['rowid']) ) {
+      $questionUpdateStatement = $questionsDb->prepare('UPDATE questions SET question_text = (:question_text), answer = (:answer), value = (:value) WHERE rowid = (:rowid)');
+      $questionUpdateStatement->bindValue('question_text', $_POST['question-text']);
+      $questionUpdateStatement->bindValue('answer', $_POST['answer']);
+      $questionUpdateStatement->bindValue('value', $_POST['value']);
+      $questionUpdateStatement->bindValue('rowid', $_POST['rowid']);
+      $questionUpdateResult = $questionUpdateStatement->execute();
+      if(!$questionUpdateResult) {
+        echo "An error occurred!";
+        return 0;
+      }
+    }
+    else {
+      $questionInsertStatement = $questionsDb->prepare('INSERT INTO questions (question_text, answer, value, category_id) VALUES ((:question_text), (:answer), (:value), (:categoryId))');
+      $questionInsertStatement->bindValue('question_text', $_POST['question-text']);
+      $questionInsertStatement->bindValue('answer', $_POST['answer']);
+      $questionInsertStatement->bindValue('value', $_POST['value']);
+      $questionInsertStatement->bindValue('categoryId', $categoryId);
+      $questionInsertResult = $questionInsertStatement->execute();
+      if(!$questionInsertResult) {
+        echo "An error occurred!";
+        return 0;
+      }
+    }
+  }
+}
+else {
+  return 0;
 }
 
 ?>
 <html>
 <head>
-  <title><?php echo $categoryTitle ?></title>
+  <title>Editing "<?php echo $categoryTitle ?>"</title>
 </head>
 <body>
 
   <h1><?php echo $categoryTitle ?></h1>
+
+  <a href="./">Categories Home</a>
 
   <table border="true">
     <tr>
@@ -39,7 +72,7 @@ if( isset($_GET['id']) && !empty($_GET['id']) ) {
     $questionsResult = $selectQuestionsStmt->execute();
     while($questionRow = $questionsResult->fetchArray(SQLITE3_ASSOC)) {
       ?>
-      <form action="./category.php" method="post">
+      <form action="./category.php?id=<?php echo $categoryId ?>" method="post">
         <input name="rowid" type="hidden" value="<?php echo $questionRow['rowid']; ?>" />
         <tr>
           <td><input name="question-text" type="text" class="form-input" size=100 value="<?php echo $questionRow['question_text']; ?>" /></td>
@@ -53,7 +86,7 @@ if( isset($_GET['id']) && !empty($_GET['id']) ) {
     }
   ?>
 
-    <form id="new-question-form" action="./category.php" method="post">
+    <form id="new-question-form" action="./category.php?id=<?php echo $categoryId ?>" method="post">
       <tr>
         <td><input name="question-text" type="text" class="form-input" size=100 placeholder="New Clue" /></td>
         <td><input name="answer" type="text" class="form-input" /></td>
