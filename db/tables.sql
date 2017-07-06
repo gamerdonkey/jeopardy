cd42a780e09e08786f9e64c62bcdce6e9b900018
gamerdonkey <gamerdonkey@gmail.com>
2017-07-06 12:48:45 -0500

    Added new tables for scoring and games.

--- db/tables.sql
+++ db/tables.sql
@@ -1,2 +1,4 @@
 CREATE TABLE categories(title TEXT);
 CREATE TABLE questions(question_text TEXT, answer TEXT, value INT, category_id INT, used INT);
+CREATE TABLE games(round_id INT, category_id INT, question_id INT);
+CREATE TABLE scores(team_name text, score INT);
